413ae2e9bafba960bf4dbb4401c2c294c73a032f
vipraxus <vipraxus.main@gmail.com>
2026-07-27 22:49:31 +0300

    24th version

--- JS/Lessons/4/main.js
+++ JS/Lessons/4/main.js
@@ -1,2 +1,7 @@
 const email_input = document.getElementById("email_input")
-const email_input = document.getElementById("password_input")
+const password_input = document.getElementById("password_input")
+const signin_button = document.getElementById("signin_button")
+
+signin_button.onclick = function() {
+    console.log("Член")
+}
